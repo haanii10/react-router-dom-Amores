acff968f4885e1c72b7f6de6da4d44c80a7d9838
Honeyleth A. Amores <amoreshoneyleth1803@gmail.com>
2023-11-16 13:32:08 -0800

    First Commit

---
+++ react-router-dom-Amores/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <span><a className='navtext' href='/'>LOGIN</a></span>
+        <span><a className='navtext' href='/register'>REGISTER</a></span>
+    </div>
+  )
+}
+
+export default Nav
